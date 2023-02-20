f3fd0520229f058ec316d3be6fe31d7439044aa1
aoyama-micron <j.aoyama@micron-inc.co.jp>
2023-02-20 09:22:28 +0900

    :zap: チェック時にstyle変更機能を追加

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ $('#addTodo')
 	.append('<li><input type="checkbox">' + inputTodo + '</li>');
 
 	$('input').val('');
-})
+});
+
+$(document)
+.on('change', 'input[type=checkbox]', function(){
+	if($(this).is(':checked')){
+		$(this).parent().addClass('text-decoration');
+	} else {
+		$(this).parent().removeClass('none-text-decoration');
+	}
+});
